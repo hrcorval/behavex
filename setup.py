b9a0f95a1f32491b341b12c52eeaa6b6cede99d1
hrcorval <hernan@boostup.ai>
2021-06-04 20:04:14 -0300

    Updating to version 1.3.4

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='behavex',
-    version='1.3.3',
+    version='1.3.4',
     python_requires='>=3',
     author='Hernan Rey',
-    author_email='hernanrey@gmail.com',
+    author_email='behavex_users@googlegroups.com',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     description='Agile test wrapper on top of Behave (BDD).',
