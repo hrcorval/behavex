47d101e8c380566d9ff88fab33d465939465ecdd
anibalinn <hernanrey99@hotmail.com>
2022-06-03 15:41:18 -0300

    Updating to version 1.5.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='behavex',
-    version='1.5.10',
+    version='1.5.11',
     python_requires='>=3.5',
     author='Hernan Rey',
     author_email='behavex_users@googlegroups.com',
