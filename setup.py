f08138fc5e0d82818db43fd836402857844b2273
anibalinn <hernanrey99@hotmail.com>
2023-07-24 19:52:06 -0300

    Updating release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='behavex',
-    version='2.0.2',
+    version='2.0.3',
     python_requires='>=3.5',
     author='Hernan Rey',
     author_email='behavex_users@googlegroups.com',
