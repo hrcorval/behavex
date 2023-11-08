7175ae8de5fbc751827225ad90f01cc617f1c5b5
anibalinn <hernanrey99@hotmail.com>
2023-11-07 22:57:06 -0300

    Updating package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='behavex',
-    version='3.1.1',
+    version='3.1.2',
     python_requires='>=3.5',
     author='Hernan Rey',
     author_email='behavex_users@googlegroups.com',
