5dc84728d4c5dfd4e6fd27607f070d412ac106bf
anibalinn <hernanrey99@hotmail.com>
2024-10-02 18:38:13 -0300

    Updating icons

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.0.7rc2',
+    version='4.0.7rc3',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
