c909e192ec3d9f4b1ee261eef90934e5805aff0c
anibalinn <hernanrey99@hotmail.com>
2024-11-26 19:00:11 -0300

    upgrading to 4.0.9rc3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.0.9rc1',
+    version='4.0.9rc3',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
