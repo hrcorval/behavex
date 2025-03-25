310fe901447521e9768afcd3e4a8a534102e79d4
anibalinn <hernanrey99@hotmail.com>
2025-03-25 19:03:49 -0300

    Additional improvements in minify library

--- behavex/outputs/report_html.py
+++ behavex/outputs/report_html.py
@@ -76,9 +76,7 @@ def _create_files_report(content_to_file):
             content = minify_html.minify(
                 content,
                 keep_closing_tags=True,
-                keep_spaces_between_attributes=True,
                 keep_html_and_head_opening_tags=True,
-                ensure_spec_compliant_unquoted_attribute_values=True,
                 minify_js=True,
                 minify_css=True
             )
