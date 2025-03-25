f7ed8c1afd6d9dfa86b89ee1bd366c438dc730d9
anibalinn <hernanrey99@hotmail.com>
2025-03-25 12:44:49 -0300

    Printing the execution status

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,3 +1,8 @@
 
 def before_scenario(context, scenario):
     context.progress_bar = False
+
+
+def after_scenario(context, scenario):
+    # print the scenario name and the execution status
+    print(f"Scenario: {scenario.name} - Execution Status: {context.scenario_status}")
