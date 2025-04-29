6ccc5bde9d90056213eaecdffc54b2d599bfe4b4
anibalinn <hernanrey99@hotmail.com>
2025-04-29 14:48:50 -0300

    Adding allure-python-commons to behavex dependencies

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     install_requires=[
         'behave==1.2.6',
         'behavex-images>=3.0.10',
+        'allure-python-commons',
         'jinja2',
         'configobj',
         'minify-html',
