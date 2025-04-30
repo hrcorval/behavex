bf5efc11579b925b5ae18d297551388f1ec22f75
anibalinn <hernanrey99@hotmail.com>
2025-04-30 15:13:49 -0300

    Updating library version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.2.0rc2',
+    version='4.2.0rc3',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
