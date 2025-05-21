b28459400c477df214016a599a087e3a755f8958
anibalinn <hernanrey99@hotmail.com>
2025-05-21 15:03:38 -0300

    Adding missing dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.2.0rc4',
+    version='4.2.0rc5',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
