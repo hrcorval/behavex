7571fbe2cbdd9a2a6a20686578d63987fa3b3605
anibalinn <hernanrey99@hotmail.com>
2025-05-21 18:53:31 -0300

    updating rc version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.2.0rc6',
+    version='4.2.0rc7',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
