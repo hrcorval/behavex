21e5bb231e922c54ebb485c933c3871b2dfd599b
anibalinn <hernanrey99@hotmail.com>
2025-06-05 19:20:28 -0300

    Updating setup information

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.2.0',
+    version='4.2.1rc1',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
