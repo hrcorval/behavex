ee6b21875d811e742a7eef141f75f7adea8e0c26
anibalinn <hernanrey99@hotmail.com>
2025-06-13 19:46:53 -0300

    Updating release version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.2.2rc2',
+    version='4.2.2rc3',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
