353caffbdfd7c530133b033f5515bc6bffb457d1
anibalinn <hernanrey99@hotmail.com>
2025-07-04 21:27:37 -0300

    Generating the the final release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.2.4rc3',
+    version='4.2.4',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
