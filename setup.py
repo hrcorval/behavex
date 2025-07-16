1bc79657e7ea74df55182ab7175bfb4f38ad4caf
anibalinn <hernanrey99@hotmail.com>
2025-07-16 09:45:24 -0300

    Dependency version improvement

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     },
     install_requires=[
         'behave==1.2.6',
-        'behavex-images==3.2.2',
+        'behavex-images>=3.2.2',
         'allure-python-commons',
         'jinja2',
         'configobj',
