2b695b33d70e6708571d1e7060970cb4966872ef
anibalinn <hernanrey99@hotmail.com>
2025-08-04 16:00:02 -0300

    Updating behavex version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='behavex',
-    version='4.4.1',
+    version='4.4.2rc1',
     license="MIT",
     platforms=['any'],
     python_requires='>=3.5',
